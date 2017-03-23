ddfccbdd3a59388cd4cff266141c8e8cb4562b2c
Bang, Yong Hee <bangyh@gmail.com>
2017-03-23 14:24:22 +0000

    Using Get

--- headlines.py
+++ headlines.py
@@ -1,21 +1,26 @@
 import feedparser
 from flask import Flask
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
 
 RSS_FEED = {'bbc' : "http://feeds.bbci.co.uk/news/rss.xml",
             'cnn' : "http://rss.cnn.com/rss/edition.rss",
             'fox' : "http://feeds.foxnews.com/foxnews/latest",
-            'iol' : "http://iol.co.za/cmlink/1.640"}
+            'iol' : "http://iol.co.za/cmlink/1.640",
+            'jtbc': "http://fs.jtbc.joins.com/RSS/newsflash.xml"}
 
 BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication='bbc'):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_FEED:
+        publication='jtbc'
+    else:
+        publication=query.lower()
     feed = feedparser.parse(RSS_FEED[publication])
-    first_article = feed['entries'][0]
     return render_template("home.html", articles=feed['entries'])
     
 if __name__ == '__main__':
